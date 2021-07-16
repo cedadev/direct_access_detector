0b8288e0ef33e42077611e9947ec6bbd3ad49ad4
Sam Pepler <sam.pepler@stfc.ac.uk>
2021-07-16 12:46:36 +0100

    working version with ingest control

--- access_detector.py
+++ access_detector.py
@@ -4,8 +4,10 @@ from stat import S_ISLNK, S_ISDIR
 import urllib.request
 import sys
 import time
+import datetime
 
 STORE = "/datacentre/processing3/access_detector/last_logs"
+EVENTS = "/datacentre/processing3/access_detector/events"
 FILESETINFOURL = 'https://cedaarchiveapp.ceda.ac.uk/fileset/%s/info'
 SPOTLISTURL = 'https://cedaarchiveapp.ceda.ac.uk/fileset/download_conf/'
 
@@ -16,6 +18,7 @@ class FileSetListing:
         self.current = 0
         self.prev_fh = None
         self.next_fh = None
+        self.events_fh = None
         self.filesetinfo = {}
         self.last_audit_time = 0 
         self.current_loc = None
@@ -26,6 +29,8 @@ class FileSetListing:
         return os.path.join(STORE, f"{self.spot}_____tmp.txt")
     def store_bak_file(self):
         return os.path.join(STORE, f"{self.spot}_____bak.txt")
+    def event_file(self):
+        return os.path.join(EVENTS, f"{self.spot}.events.txt")
 
     def last_done(self):
         if not os.path.exists(self.store_file()):
@@ -36,6 +41,8 @@ class FileSetListing:
         with urllib.request.urlopen(FILESETINFOURL % self.spot) as f:
             self.filesetinfo = json.loads(f.read())
             self.current_loc = self.filesetinfo["storage_path"]
+            if "last_audit_starttime" in self.filesetinfo: 
+                self.last_audit_time = self.filesetinfo["last_audit_starttime"]
             if "last_audit_endtime" in self.filesetinfo: 
                 self.last_audit_time = self.filesetinfo["last_audit_endtime"]
       
@@ -50,13 +57,13 @@ class FileSetListing:
 
     def forwardto(self, path):
         # forward to the first record of path or a record of something greater than path
-        path_from_file, atime = self.readline() 
+        path_from_file, atime = self._readline() 
         while path > path_from_file:
             # print(f" - Skipping path = {path}   path_from_file = {path_from_file}")
-            path_from_file, atime = self.readline() 
+            path_from_file, atime = self._readline() 
         return path_from_file, int(atime)
 
-    def readline(self):
+    def _readline(self):
         line = self.prev_fh.readline()
         if line == '':
                 return "~~~~~~~~", 0
@@ -66,6 +73,7 @@ class FileSetListing:
     def open(self):
         self.open_prev()
         self.open_next()
+        self.events_fh = open(self.event_file(), "a")
 
     def open_prev(self):
         if not os.path.exists(self.store_file()):
@@ -81,12 +89,23 @@ class FileSetListing:
     def close(self):
         self.prev_fh.close()
         self.next_fh.close()
+        self.events_fh.close()
         os.rename(self.store_file(), self.store_bak_file())
         os.rename(self.store_tmp_file(), self.store_file())
 
     def add(self, path, atime):
         self.next_fh.write(f"{path}|{atime}\n")
-   
+
+    def make_event(self, path, atime, size):
+        etime = datetime.datetime.fromtimestamp(atime).isoformat()
+        path = path[2:] 
+        path = os.path.join(self.filesetinfo["logical_path"], path)
+        directory, name = os.path.split(path) 
+        event = {"directory": directory, "name": name, "size": size, "event_time": etime, 
+                 "item_type": "FILE", "event_type": "direct_access"}   
+        self.events_fh.write(json.dumps(event) + "\n")
+        self.events_fh.flush()
+
     def find_access_events(self, directory=None):
         if directory is None:
             self.get_filesetinfo() 
@@ -104,39 +123,31 @@ class FileSetListing:
 
             # skip links
             if S_ISLNK(stat.st_mode):
-                #print("LINK") 
                 continue
+
             # recurse dirs
             if S_ISDIR(stat.st_mode):
-                #print("DIR") 
                 self.find_access_events(path)
                 continue
 
-            # if the file is not in the file system but is on the list then it's removed.
-            #  A removed file is not an access event and is not added to the next list.
-            prev_path, prev_atime = self.forwardto(path)
-            # print(path, atime, prev_path, prev_atime)
-
-            if prev_path != path:
-                # if the file is in the filesystem and not the list its new. 
-                #  A new file is an access event only if the access time is 1 day post mtime.
-                if atime > mtime + 24*3600 and self.last_audit_time == 0:
-                    event_time = atime
-                    directory = ?
-                    name = ?
-                    item_type = "FILE"
-                    size = stat.st_size
-                    event_type = "direct_access"
-                    print(f"{path}:    access after new file creation event")
-            else:
-                # If the file is on the list and in the filesystem then 
-                #  if the atime is different from the previous one (allowing for audit times, backups?) then 
-                #    an access event has happened and the file goes on the next list.
-                if atime > prev_atime and atime > self.last_audit_time:
-                     print(f"{path}:   access event")
-
-            #  The file goes on the next list.             
+            # its a file from here on. The file goes on the next list.
             self.add(path, atime)
+
+            # Not an access event if audit time after atime.  
+            if atime < self.last_audit_time:
+                continue
+
+            # Move forward in the previous list to the current file or the file after where is would have been.
+            prev_path, prev_atime = self.forwardto(path)
+
+            # if the file is new and has been access at least a day after deposit then its an access event.
+            new_file_accessed = prev_path != path and atime > mtime + 24*3600
+
+            # if a privious file has been accessed then its an access event  
+            old_file_accessed = prev_path == path and atime > prev_atime
+
+            if new_file_accessed or old_file_accessed:
+                self.make_event(path, atime, stat.st_size)
 
         if directory == ".":
             self.close()
